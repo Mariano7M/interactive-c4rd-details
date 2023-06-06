4675b3e2bf2c4de504fe89672bc0dec724049c79
Mariano <marinhoted@gmail.com>
2023-06-06 14:46:58 -0600

    Feat: Removing default elements and styles to start adding new elements for the interactive card details

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThankYou } from './components/thank-you';
+import { CardForm } from './components/card-form';
+import { BankCards } from './components/bank-cards';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const showTankYou = false;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="main main--background">
+      <BankCards />
+      <section className="main__form">
+        <CardForm />
+      </section>
+      {showTankYou && <ThankYou />}
+    </div>
+  );
 }
 
-export default App
+export default App;
