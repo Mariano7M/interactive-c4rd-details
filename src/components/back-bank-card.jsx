ecdefc6caae3213f3d6d3a994039d7e57e85216e
Mariano <marinhoted@gmail.com>
2023-06-06 15:01:03 -0600

    Feat: Adding back bank card component for mobile devices

---
+++ src/components/back-bank-card.jsx
@@ -0,0 +1,14 @@
+// import PropTypes from 'prop-types';
+import './back-bank-card.css';
+
+export function BackBankCard() {
+  return (
+    <div className="back-bank-card">
+      <span className="back-bank-card__cvc">000</span>
+    </div>
+  );
+}
+
+// BackBankCard.propTypes = {
+//   cvc: PropTypes.string.isRequired,
+// };
